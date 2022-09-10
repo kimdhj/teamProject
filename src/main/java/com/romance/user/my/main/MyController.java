ef751b600a0e28688f3e30188d9e0ec689dcc873
Donghyun Kim <hyunhahaha13@gmail.com>
2022-09-10 19:33:12 +0900

    기능추가

--- src/main/java/com/romance/user/my/main/MyController.java
+++ src/main/java/com/romance/user/my/main/MyController.java
@@ -1,20 +1,33 @@
 package com.romance.user.my.main;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.romance.admin.login.CheckToken;
+import com.romance.security.JwtUtils;
+import com.romance.user.login.UserVO;
 
 @Controller
 @RequestMapping("/")
 public class MyController {
 	
+	
 	@GetMapping("myMain.do")
-	public String myMain() {
+	public String myMain(HttpSession session, JwtUtils util) {
+		
+		if(CheckToken.isToken(session, util) == 1) {
+			System.out.println("안녕하십니까?");
+		}
+		
 		return "my_Main";
 	}
 	
 	@GetMapping("myPassword.do")
 	public String myPassword() {
+		
 		return "my_Password";
 	}
 }
