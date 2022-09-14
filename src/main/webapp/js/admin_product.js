f21081a8c497a5c24efdb8c86637633a95c2960e
브랜치가머지 <rlagptjd2002@naver.com>
2022-09-14 19:54:56 +0900

    관리자 상품관리 READ 페이징 완성

--- src/main/webapp/js/admin_product.js
+++ src/main/webapp/js/admin_product.js
@@ -30,18 +30,23 @@ function tableDelete(obj) {
 		
 //검색버튼클릭시 - 테이블갱신 - 페이징 갱신		
 $("#search_btn").click(function(e){
-	let count=0;
+	 
+	 console.log($("#Hpage").val());
+	 console.log($("#sort").val());
+	 console.log($("#searchThing").val());
+	 
+	 $("#Hpage").val(1);
 	 
 	$.ajax({
 			url: "/ajax_search.mdo",
 			type: 'POST',
 			data: {
-				page: $("#page").val(),
-				sort: $("#sort").val(),
-				thing: $("#searchThing").val()
+				Hpage: Number($("#Hpage").val()),
+				Hsort: $("#sort").val(),
+				Hthing: $("#searchThing").val()
 			},
 			success: function(data) {
-				console.log(data);
+				console.log("검색버튼 결과"+data);
 				let con="";
 				data.map((pro) => {
 				con+=
@@ -85,10 +90,17 @@ $("#search_btn").click(function(e){
 				});
 				$("tbody").html(con);
 				
+				
+				console.log($("#now").text());
+				console.log($("#sort").val());
+				console.log($("#searchThing").val());
+				
+				$("#Hsort").val($("#sort").val());
+				$("#Hthing").val($("#searchThing").val());
+				
 				countt();
 				
-				
-				$("#page").val($("#now").text());
+				$("#Hpage").val($("#now").text());
 				
 			},
 			error: function(jqXHR, textStatus, errorThrown) {
@@ -106,40 +118,117 @@ $(document).on('click', '#pppp #reload a', viewview);
 
 //페이징만 이동
 function viewview(v){
-	console.log($(v.target).text());
-			$("#page").val($(v.target).text());
-	countt();
+				
+				console.log($(v.target).text());				
+				console.log($("#sort").val());
+				console.log($("#Hpage").val());
+				console.log($("#searchThing").val());
+				
+				if($(v.target).text()=="«"){
+				$("#Hpage").val(parseInt(($("#Hpage").val()-1)/10)*10);
+				}else if($(v.target).text()=="»"){
+				$("#Hpage").val((parseInt(($("#Hpage").val()-1)/10)+1)*10+1);				
+				}else{
+				$("#Hpage").val($(v.target).text());
+				}
+				
+				$("#Hsort").val($("#sort").val());
+				$("#Hthing").val($("#searchThing").val());
+				
+				countt();
+				
+	$.ajax({
+			url: "/ajax_search.mdo",
+			type: 'POST',
+			data: {
+				Hpage: Number($("#Hpage").val()),
+				Hsort: $("#Hsort").val(),
+				Hthing: $("#Hthing").val()
+			},
+			success: function(data) {
+				console.log("뷰뷰 결과"+data);
+				let con="";
+				data.map((pro) => {
+				con+=
+				`<tr>
+								<td><input type="checkbox" id=""></td>
+								<td>
+									<p class="rowColumn" contenteditable="false" data-default="${pro.book_seq }">${pro.book_seq }</p>
+								</td>
+								<td>
+									<p class="rowColumn" contenteditable="false" data-default="${pro.book_title }">${pro.book_title }</p>
+								</td>
+								<td>
+									<p class="rowColumn" contenteditable="false" data-default="${pro.author_seq } . ${pro.book_author}">${pro.author_seq } . ${pro.book_author}</p>
+								</td>
+								<td>
+									<p class="rowColumn" contenteditable="false"
+										data-default="${pro.book_publish }">${pro.book_publish }</p>
+								</td>
+								<td>
+									<p class="rowColumn" contenteditable="false" data-default="${pro.book_remain }">${pro.book_remain }</p>
+								</td>
+								<td>
+									<p class="rowColumn" contenteditable="false"
+										data-default="${pro.book_price.toLocaleString('ko-KR') }원">${pro.book_price.toLocaleString('ko-KR') }원</p>
+								</td>
+								<td>
+									<p class="rowColumn" contenteditable="false" data-default="${pro.category_num }">${pro.category_num }</p>
+								</td>
+								<td>
+									<p class="rowColumn" contenteditable="false" data-default="${pro.book_publish_date }">${pro.book_publish_date }</p>
+								</td>
+								<td>
+									<p class="rowColumn" contenteditable="false" data-default="${pro.book_sellCount }">${pro.book_sellCount }</p>
+								</td>
+								<td>
+									<button type="button" id="del_one">삭제</button>
+								</td>
+							</tr>
+				`;
+
+				});
+				$("tbody").html(con);
+				
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				console.log('error while post');
+			}
+		});
+
 }
 
 
 //게시글수 세오고 페이징 갱신
 function countt() {
-	let count = 0;
 	$.ajax({
 		url: "/ajax_count.mdo",
 		method: "POST",
 		dataType: "JSON",
 		async: false,
 		data:{
-			page: $("#page").val(),
-			sort: $("#sort").val(),
-			thing: $("#searchThing").val()
+			Hpage: Number($("#Hpage").val()),
+			Hsort: $("#Hsort").val(),
+			Hthing: $("#Hthing").val()
 		},
 		success: function(co){
+		
+			console.log("count 결과값"+co);
+			
 			//게시글 수
-			count = co;
+			let count = co;
 			
 			//현재 페이지
-			let page = $("#page").val();
+			let page = $("#Hpage").val();
 			//최대 페이지
 			let fullpage = 0;
 			if(count%20 ==0) {
-				fullpage=count/20;
+				fullpage=parseInt(count/20);
 			}else if(count%20 !=0) {
-				fullpage=count/20 +1;
+				fullpage=parseInt(count/20) +1;
 			}
 			//최대페이지 나누기 10
-			let wherepage = parseInt(page/10);
+			let wherepage = parseInt((page-1)/10);
 			// 현재 시작 페이지
 			let startpage = wherepage*10 +1;
 			// 현재 끝 페이지
