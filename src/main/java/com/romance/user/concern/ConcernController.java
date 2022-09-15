50d54ad299915aaa69db7d0cf1d6f46af320d953
qjawns0222 <ko02222@naver.com>
2022-09-15 11:23:03 +0900

    관심작가

--- src/main/java/com/romance/user/concern/ConcernController.java
+++ src/main/java/com/romance/user/concern/ConcernController.java
@@ -1,6 +1,7 @@
 package com.romance.user.concern;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.romance.security.JwtUtils;
@@ -36,8 +38,76 @@ public class ConcernController {
     }
   }
   @GetMapping("/mywriter.do")
-  public String myConcertWriter(JwtUtils util,HttpSession session,Model model) throws IOException {
-    UserVO vo=util.getuser(session);
+  public String myConcertWriter(ConcernSearchVO vo,JwtUtils util,HttpSession session,String pages,Model model) throws IOException  {
+   UserVO vou= util.getuser(session);
+   if(vou==null) {
+     return "redirect:index.do";
+   }
+   
+   vo.setUser_id(vou.getUser_id());
+if(vo.getPage()==0) {
+  vo.setPage(1);
+}
+   
+    int count=ser.concerncount(vo);
+    List<ConcernWriterVO> list=ser.concernall(vo);
+    list=ser.getautor(list);
+    System.out.println(count);
+    System.out.println(list);
+    
+    if(count%5==0) {
+      count-=1;
+    }
+    if(vo.getPage()<3) {
+      model.addAttribute("startpage", 1);
+    }else {
+      model.addAttribute("startpage", vo.getPage()-2);
+    }
+    if(vo.getPage()+2>(count/5)+1) {
+      System.out.println(count);
+      model.addAttribute("endpage", count/5+1);
+    }else {
+      System.out.println("el"+count);
+      model.addAttribute("endpage",vo.getPage()+2);
+    }
+    model.addAttribute("count",count-((vo.getPage()-1)*5));
+    model.addAttribute("maxpage",(int)count/5+1);
+    model.addAttribute("vo",vo);
+    model.addAttribute("list",list);
+    System.out.println(vo.getPage());
+    
     return "my_ConcertWriter";
   }
+  @GetMapping("concernlist.do")
+  @ResponseBody
+  public List<ConcernWriterVO>concernlist(ConcernSearchVO vo,JwtUtils util,HttpSession session) throws IOException{
+    UserVO vou= util.getuser(session);
+    vo.setUser_id(vou.getUser_id());
+    List<ConcernWriterVO> list=ser.concernall(vo);
+    list=ser.getautor(list);
+    return list;
+  }
+  @GetMapping("concerncount.do")
+  @ResponseBody
+  public int concerncount(ConcernSearchVO vo,JwtUtils util,HttpSession session) throws IOException{
+    UserVO vou= util.getuser(session);
+    vo.setUser_id(vou.getUser_id());
+    if(vo.getPage()==0) {
+      vo.setPage(1);
+    }
+    int count=ser.concerncount(vo);
+    if(count%5==0) {
+      count-=1;
+    }
+    return count;
+  } 
+  @GetMapping("concerndel.do")
+  @ResponseBody   
+  public void concerndel(@RequestParam("author_seq[]")List<Integer> author_seq,JwtUtils util,HttpSession session) throws IOException {
+    UserVO vo=util.getuser(session);
+    ser.concerndel(author_seq, vo.getUser_id());
+  }
+
+ 
+  
 }
