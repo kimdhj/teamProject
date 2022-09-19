bbdb6cee113ac1f7f96b612e61df101fd740eb2f
qjawns0222 <ko02222@naver.com>
2022-09-20 01:14:50 +0900

    결제 부분 취소

--- src/main/java/com/romance/admin/trade/TradeDAO.java
+++ src/main/java/com/romance/admin/trade/TradeDAO.java
@@ -6,6 +6,12 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.romance.admin.coupon.CouponVO;
+import com.romance.user.coupon.MyCouponVO;
+import com.romance.user.login.UserVO;
+import com.romance.user.orders.OrdersVO;
+import com.romance.user.points.MyPointsVO;
+
 @Repository
 public class TradeDAO {
   @Autowired
@@ -32,4 +38,38 @@ public TradeJoinVO ordersdetail(int orders_seq) {
     
     return sql.selectOne("trade.ordersdetail",orders_seq);
   }
+public void orderbookdel(TradeJoinVO vo) {
+  sql.delete("order.orderbookdel",vo);
+  
+}
+public void orderupdate(TradeJoinVO vo) {
+  sql.update("order.orderupdate",vo);
+  
+}
+public OrdersVO ordersel(TradeJoinVO vo) {
+  return sql.selectOne("order.ordersel",vo);
+  
+}
+public void ordersdel(TradeJoinVO vo) {
+  sql.delete("order.ordersdel",vo);
+}
+public int chebooklist(TradeJoinVO vo) {
+  return sql.selectOne("order.chebooklist",vo);
+  
+}
+public void couponin(MyCouponVO vo) {
+  sql.insert("usercoupon.couponin",vo);
+  
+}
+public void orderpointupdate(TradeJoinVO vo) {
+  sql.update("order.orderpointupdate",vo);
+  
+}
+public void pointsin(MyPointsVO vo) {
+  sql.insert("points.pointsin",vo);
+}
+public void refundpoints(UserVO vo) {
+  sql.update("user.refundpoints",vo);
+}
+
 }
